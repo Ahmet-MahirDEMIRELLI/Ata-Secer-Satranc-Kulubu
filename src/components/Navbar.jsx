21996d29250be54255ff55bc465522aa846f71eb
Ahmet-MahirDEMIRELLI <a.mahir.demirelli@gmail.com>
2024-07-20 12:47:22 +0300

    updated announcment detail and added navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import Logo from '../assets/logo.png'
 import { NavLink } from 'react-router-dom'
 import '../styles/Navbar.css'
+import {useRef} from 'react'
 export default function Navbar() {
+  const sideBarRef = useRef(null);
+  const handleClick = () => {
+    sideBarRef.current.classList.toggle('openSideBar');
+  }
   return (
-    <div className='navbar'>
+    <header className='navbar'>
+      <div className='links'>
         <div className='main'>
-            <img src={Logo} alt="" />
+          <img src={Logo} alt="" />
             <div className='mainLink'>
                 <NavLink to={"/Ata-Secer-Satranc-Kulubu/"}>Anasayfa</NavLink>
                 <NavLink to={"/Ata-Secer-Satranc-Kulubu/announcements"}>Duyurular</NavLink>
@@ -13,6 +19,25 @@ export default function Navbar() {
                 <NavLink to={"/Ata-Secer-Satranc-Kulubu/contact"}>İletişim</NavLink>
             </div>
         </div>
-    </div>
+      </div>
+
+      <div className="hamburger" onClick={handleClick}>
+        <div className='image'>
+          <img src={Logo} alt="" />
+        </div>
+        <div className='bars'>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>    
+      </div>
+      <div ref={sideBarRef} className='mobileNav'>
+        <NavLink to={"/Ata-Secer-Satranc-Kulubu/"}>Anasayfa</NavLink>
+        <NavLink to={"/Ata-Secer-Satranc-Kulubu/announcements"}>Duyurular</NavLink>
+        <NavLink to={"/Ata-Secer-Satranc-Kulubu/about"}>Hakkımızda</NavLink>
+        <NavLink to={"/Ata-Secer-Satranc-Kulubu/contact"}>İletişim</NavLink>
+      </div>
+    </header>
+    
   )
 }
